05c12b2c0e64a44fdf45eb5e10f2f65eeb4b0ce8
VictorMauroy <mauroy.victor@gmail.com>
2023-06-06 15:38:38 +0200

    Feat: :sparkles: :bug: fix an error in the creation of the tree and add a function to return the tree with the depth browse method

--- tree.py
+++ tree.py
@@ -1,9 +1,8 @@
 class Tree :
 
-    def __init__(self, value, parent_branch, childs = []) :
+    def __init__(self, value, childs = []) :
         self.value = value
-        self.child_branchs = childs
-        self.parent_branch = parent_branch
+        self.child_branchs:list[Tree] = childs
     
     def add_child(self, value) :
         """Add a child to the current tree"""
@@ -12,7 +11,7 @@ class Tree :
 
     def is_leaf(self) -> bool :
         """Whether the current tree has childs branchs or not"""
-        return True if len(self.child_branchs) == 0 else False
+        return not self.child_branchs
     
     def find_leaf(self) :
         """Return the first lead founded"""
@@ -25,28 +24,41 @@ class Tree :
         """Return the count of childs in the current tree"""
         return len(self.child_branchs)
     
-    def browse_depth(self) -> list :
-        """Return the childs with their values with the depth browse method"""
-    
+    def browse_depth(self, list_of_branchs:list = []) -> list :
+        """Return the childs with their values by using the depth browse method"""
+        print(f"{self.value} is {'a leaf' if self.is_leaf() else 'not a leaf'}")
+        
+        if not self.is_leaf() :
+            for branch in self.child_branchs :
+                branch.browse_depth(list_of_branchs)
+        
+        list_of_branchs.append(self.value)
+
+        return list_of_branchs
 
     def browse_width(self) -> list :
-        """Return the childs with their values with the width browse method"""
+        """Return the childs with their values by using the width browse method"""
 
 
 root_tree = Tree(0, [])
-root_tree.add_child(1, root_tree)
-root_tree.add_child(2, root_tree)
+root_tree.add_child(1)
+root_tree.add_child(2)
 count = 3
+
+# Create 3 branchs in each branch of the root
 for branch in root_tree.child_branchs :
+    
     for i in range(3) :
-        branch.add_child(count, branch)
+        branch.add_child(count)
         count += 1
 
-    for branch2 in root_tree.child_branchs :
-        for elt in range(branch2.child_branchs) :
-            elt.add_child(count, elt)
-            count += 1
-            elt.add_child(count, elt)
-            count += 1
+# Create 2 branch for each branch in each branch of the root
+for branch in root_tree.child_branchs :
+    
+    for elt in branch.child_branchs :
+        elt.add_child(count)
+        count += 1
+        elt.add_child(count)
+        count += 1
 
-
+print(root_tree.browse_depth())
